e78264e0c1ee99f87fe174ab1cb8c45dcb44ae81
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-04-07 22:01:56 -0400

    Main File Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,240 +1,25 @@
+import '../screens/authentication_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-enum TaskPriority { low, medium, high }
-
-extension PriorityExtension on TaskPriority {
-  String get name {
-    switch (this) {
-      case TaskPriority.low:
-        return 'Low';
-      case TaskPriority.medium:
-        return 'Medium';
-      case TaskPriority.high:
-        return 'High';
-    }
-  }
-
-  Color get color {
-    switch (this) {
-      case TaskPriority.low:
-        return Colors.green;
-      case TaskPriority.medium:
-        return Colors.orange;
-      case TaskPriority.high:
-        return Colors.red;
-    }
-  }
-}
-
-class Task {
-  String name;
-  bool isCompleted;
-  TaskPriority priority;
-
-  Task({required this.name, this.isCompleted = false, this.priority = TaskPriority.medium});
-}
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Task Manager',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const TaskListScreen(),
-    );
-  }
-}
-
-class TaskListScreen extends StatefulWidget {
-  const TaskListScreen({Key? key}) : super(key: key);
-
-  @override
-  State<TaskListScreen> createState() => _TaskListScreenState();
-}
-
-class _TaskListScreenState extends State<TaskListScreen> {
-  final List<Task> _tasks = [];
-  final TextEditingController _taskController = TextEditingController();
-  TaskPriority _selectedPriority = TaskPriority.medium;
-
-  void _sortTasks() {
-    _tasks.sort((a, b) {
-      int priorityComparison = b.priority.index.compareTo(a.priority.index);
-      if (priorityComparison != 0) {
-        return priorityComparison;
-      }
-      if (a.isCompleted != b.isCompleted) {
-        return a.isCompleted ? 1 : -1;
-      }
-      return a.name.compareTo(b.name);
-    });
-  }
-
-  void _addTask() {
-    if (_taskController.text.isNotEmpty) {
-      setState(() {
-        _tasks.add(
-          Task(name: _taskController.text, priority: _selectedPriority),
-        );
-        _sortTasks();
-        _taskController.clear();
-        _selectedPriority = TaskPriority.medium;
-      });
-    }
-  }
-
-  void _toggleTaskCompletion(int index) {
-    setState(() {
-      _tasks[index].isCompleted = !_tasks[index].isCompleted;
-      _sortTasks();
-    });
-  }
-
-  void _deleteTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
-  }
-
-  @override
-  void dispose() {
-    _taskController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Task Manager')),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _taskController,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter a Task',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                DropdownButton<TaskPriority>(
-                  value: _selectedPriority,
-                  onChanged: (TaskPriority? newValue) {
-                    if (newValue != null) {
-                      setState(() {
-                        _selectedPriority = newValue;
-                      });
-                    }
-                  },
-                  items: TaskPriority.values.map((TaskPriority priority) {
-                    return DropdownMenuItem(
-                      value: priority,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 5,
-                        ),
-                        decoration: BoxDecoration(
-                          color: priority.color.withOpacity(0.2),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Text(
-                          priority.name,
-                          style: TextStyle(
-                            color: priority.color,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _addTask,
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: _tasks.isEmpty
-                ? const Center(
-                    child: Text(
-                      'No tasks yet. To get started add a task!',
-                      style: TextStyle(fontSize: 16, color: Colors.grey),
-                    ),
-                  )
-                : ListView.builder(
-                    itemCount: _tasks.length,
-                    itemBuilder: (context, index) {
-                      final task = _tasks[index];
-                      return Card(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 8,
-                        ),
-                        elevation: 2,
-                        child: ListTile(
-                          leading: Checkbox(
-                            value: task.isCompleted,
-                            onChanged: (_) => _toggleTaskCompletion(index),
-                          ),
-                          title: Text(
-                            task.name,
-                            style: TextStyle(
-                              decoration: task.isCompleted
-                                  ? TextDecoration.lineThrough
-                                  : null,
-                              color: task.isCompleted
-                                  ? Colors.grey
-                                  : Colors.black,
-                            ),
-                          ),
-                          subtitle: Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8,
-                              vertical: 2,
-                            ),
-                            decoration: BoxDecoration(
-                              color: task.priority.color.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            constraints: const BoxConstraints(maxWidth: 120),
-                            child: Text(
-                              'Priority: ${task.priority.name}',
-                              style: TextStyle(
-                                color: task.priority.color,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete, color: Colors.red),
-                            onPressed: () => _deleteTask(index),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-          ),
-        ],
-      ),
+      title: 'Firebase Auth App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const AuthenticationScreen(),
     );
   }
 }
